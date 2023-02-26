71c513d0992051126ecff7b9ae06fd73c33020a0
hashankur <aeonian.adroit24@gmail.com>
2023-02-26 15:37:27 +0530

    refactor(fish): remove garuda themed lambda line

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,75 +14,6 @@ set fish_greeting
 ## Source .profile to apply its values
 #source ~/.profile
 
-## Lambda theme https://github.com/hasanozgan/theme-lambda
-function fish_prompt
-  # Cache exit status
-  set -l last_status $status
-
-  # Just calculate these once, to save a few cycles when displaying the prompt
-  if not set -q __fish_prompt_hostname
-    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-  end
-  if not set -q __fish_prompt_char
-    switch (id -u)
-      case 0
-	set -g __fish_prompt_char '#'
-      case '*'
-	set -g __fish_prompt_char (set_color F00; echo λ)
-    end
-  end
-
-  # Setup colors
-  #use extended color pallete if available
-#if [[ $terminfo[colors] -ge 256 ]]; then
-#    turquoise="%F{81}"
-#    orange="%F{166}"
-#    purple="%F{135}"
-#    hotpink="%F{161}"
-#    limegreen="%F{118}"
-#else
-#    turquoise="%F{cyan}"
-#    orange="%F{yellow}"
-#    purple="%F{magenta}"
-#    hotpink="%F{red}"
-#    limegreen="%F{green}"
-#fi
-  set -l normal (set_color normal)
-  set -l white (set_color FFFFFF)
-  set -l turquoise (set_color 5fdfff)
-  set -l orange (set_color df5f00)
-  set -l hotpink (set_color df005f)
-  set -l blue (set_color blue)
-  set -l limegreen (set_color 87ff00)
-  set -l purple (set_color af5fff)
- 
-  # Configure __fish_git_prompt
-  set -g __fish_git_prompt_char_stateseparator ' '
-  set -g __fish_git_prompt_color 5fdfff
-  set -g __fish_git_prompt_color_flags df5f00
-  set -g __fish_git_prompt_color_prefix white
-  set -g __fish_git_prompt_color_suffix white
-  set -g __fish_git_prompt_showdirtystate true
-  set -g __fish_git_prompt_showuntrackedfiles true
-  set -g __fish_git_prompt_showstashstate true
-  set -g __fish_git_prompt_show_informative_status true 
-
-  set -l current_user (whoami)
-
-  # Line 1
-  echo -n $white'╭─'$hotpink$current_user$white' at '$orange$__fish_prompt_hostname$white' in '$limegreen(pwd|sed "s=$HOME=⌁=")$turquoise
-  __fish_git_prompt " (%s)"
-  echo
-
-  # Line 2
-  echo -n $white'╰'
-  # support for virtual env name
-  if set -q VIRTUAL_ENV
-      echo -n "($turquoise"(basename "$VIRTUAL_ENV")"$white)"
-  end
-  echo -n $white'─'$__fish_prompt_char $normal
-end
-
 # Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
 function __history_previous_command
   switch (commandline -t)
@@ -171,9 +102,9 @@ alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pac
 
 
 ## Import colorscheme from 'wal' asynchronously
-if type "wal" >> /dev/null 2>&1
-   cat ~/.cache/wal/sequences
-end
+# if type "wal" >> /dev/null 2>&1
+#    cat ~/.cache/wal/sequences
+# end
 
 ## Run paleofetch if session is interactive
 #if status --is-interactive
